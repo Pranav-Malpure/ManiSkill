67202d9275c6e32de855cd3c124c9413c5fcf372
Pranav-Malpure <pranavmalpure2020@gmail.com>
2025-11-01 13:34:20 -0700

    increased size of cube to 7cm

--- mani_skill/envs/tasks/tabletop/pick_cube.py
+++ mani_skill/envs/tasks/tabletop/pick_cube.py
@@ -42,8 +42,8 @@ class PickCubeEnv(BaseEnv):
         "xarm6_pandagripper"
     ]
     agent: Union[Panda, Fetch, XArm6Robotiq, XArm6AllegroLeft, XArm6AllegroRight, FloatingRobotiq2F85Gripper, XArm6PandaGripper]
-    cube_half_size_allegro = 0.03
-    cube_half_size = 0.03
+    cube_half_size_allegro = 0.035
+    cube_half_size = 0.035
     goal_thresh = 0.025
 
     def __init__(self, *args, robot_uids="panda", robot_init_qpos_noise=0.02, **kwargs):
